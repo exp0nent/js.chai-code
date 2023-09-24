122bc0a951fde4a84badd21ccda9d719aef797c2
exp0nent <89779251+exp0nent@users.noreply.github.com>
2023-09-24 20:57:42 +0000

    DAY 2

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,7 +2,21 @@ const accountId=144553
 let accountEmail="aatir@gmail.com"
 var accountPassword ="12345"
 accountCity="kolkata"
+let accountState;
 
-accountId=2
+//accountId=2 //not allowed
+
+
+accountEmail ="hc@hb.com"
+accountPassword ="12435"
+accountCity="Delhi"
 
 console.log(accountId);
+
+/*Prefer not to use var
+because of issue in block scope and functional scope
+*/
+ 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+ 
